01b0e811a210820bc3d8d764406b9083bfcc636b
franklingu <franklingujunchao@gmail.com>
2014-10-06 16:09:59 +0800

    remove session

--- Visual-IDE/appengine_config.py
+++
@@ -1,8 +0,0 @@
-from gaesessions import SessionMiddleware
-import os
-
-
-def webapp_add_wsgi_middleware(app):
-    key = str(os.urandom(64))
-    app = SessionMiddleware(app, cookie_key=key)
-    return app
